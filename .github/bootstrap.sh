87e981fad85f9b504e50b1629fd8286c0ef053dd
Ethan Park <87598209+Daltonicc@users.noreply.github.com>
2024-06-25 16:01:45 +0900

    Update bootstrap.sh

--- .github/bootstrap.sh
+++ .github/bootstrap.sh
@@ -5,7 +5,6 @@ SWIFT_SYNTAX_NAME="swift-syntax"
 SWIFT_SYNTAX_REPOSITORY_URL="https://github.com/apple/$SWIFT_SYNTAX_NAME.git"
 SEMVER_PATTERN="^[0-9]+\.[0-9]+\.[0-9]+$"
 WRAPPER_NAME="SwiftSyntaxWrapper"
-ARCHS=("arm64" "x86_64")
 CONFIGURATION="debug"
 DERIVED_DATA_PATH="$PWD/derivedData"
 
@@ -87,10 +86,10 @@ MODULES=(
 )
 
 PLATFORMS=(
-    # xcodebuild destination    XCFramework folder name
-    "generic/platform=macOS"          "macos"
-    "generic/platform=iOS Simulator"  "ios-simulator"
-    "generic/platform=iOS"            "ios"
+    # xcodebuild destination            XCFramework folder name          ARCHS
+    "generic/platform=macOS"           "macos"                          "arm64 x86_64"
+    "generic/platform=iOS Simulator"   "ios-simulator"                  "arm64 x86_64"
+    "generic/platform=iOS"             "ios"                            "arm64"
 )
 
 XCODEBUILD_LIBRARIES=""
@@ -98,27 +97,28 @@ PLATFORMS_OUTPUTS_PATH="$PWD/outputs"
 
 cd $SWIFT_SYNTAX_NAME
 
-for ((i = 0; i < ${#PLATFORMS[@]}; i += 2)); do
+for ((i = 0; i < ${#PLATFORMS[@]}; i += 3)); do
     XCODEBUILD_PLATFORM_NAME="${PLATFORMS[i]}"
     XCFRAMEWORK_PLATFORM_NAME="${PLATFORMS[i+1]}"
+    ARCHS="${PLATFORMS[i+2]}"
 
-    for ARCH in "${ARCHS[@]}"; do
-        OUTPUTS_PATH="${PLATFORMS_OUTPUTS_PATH}/${XCFRAMEWORK_PLATFORM_NAME}-${ARCH}"
-        LIBRARY_PATH="${OUTPUTS_PATH}/lib${WRAPPER_NAME}.a"
-        XCODEBUILD_LIBRARIES="$XCODEBUILD_LIBRARIES -library $LIBRARY_PATH"
+    OUTPUTS_PATH="${PLATFORMS_OUTPUTS_PATH}/${XCFRAMEWORK_PLATFORM_NAME}"
+    LIBRARY_PATH="${OUTPUTS_PATH}/lib${WRAPPER_NAME}.a"
+    XCODEBUILD_LIBRARIES="$XCODEBUILD_LIBRARIES -library $LIBRARY_PATH"
 
-        mkdir -p "$OUTPUTS_PATH"
+    mkdir -p "$OUTPUTS_PATH"
 
-        xcodebuild clean build \
-            -scheme $WRAPPER_NAME \
-            -configuration $CONFIGURATION \
-            -destination "$XCODEBUILD_PLATFORM_NAME" \
-            -derivedDataPath $DERIVED_DATA_PATH \
-            ARCHS=$ARCH \
-            SKIP_INSTALL=NO \
-            BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    xcodebuild clean build \
+        -scheme $WRAPPER_NAME \
+        -configuration $CONFIGURATION \
+        -destination "$XCODEBUILD_PLATFORM_NAME" \
+        -derivedDataPath $DERIVED_DATA_PATH \
+        ARCHS="$ARCHS" \
+        SKIP_INSTALL=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-        for MODULE in ${MODULES[@]}; do
+    for MODULE in ${MODULES[@]}; do
+        for ARCH in $ARCHS; do
             INTERFACE_PATH="$DERIVED_DATA_PATH/Build/Intermediates.noindex/swift-syntax.build/$CONFIGURATION*/${MODULE}.build/Objects-normal/$ARCH/${MODULE}.swiftinterface"
             if [ -f "$INTERFACE_PATH" ]; then
                 cp $INTERFACE_PATH "$OUTPUTS_PATH"
@@ -126,15 +126,15 @@ for ((i = 0; i < ${#PLATFORMS[@]}; i += 2)); do
                 echo "Warning: $INTERFACE_PATH does not exist"
             fi
         done
-
-        OBJECT_FILES="$DERIVED_DATA_PATH/Build/Intermediates.noindex/swift-syntax.build/$CONFIGURATION*/${WRAPPER_NAME}.build/Objects-normal/$ARCH/*.o"
-        if ls $OBJECT_FILES 1> /dev/null 2>&1; then
-            ar -crs "$LIBRARY_PATH" $OBJECT_FILES
-        else
-            echo "Error: No object files found for $ARCH on $XCODEBUILD_PLATFORM_NAME"
-            exit 1
-        fi
     done
+
+    OBJECT_FILES="$DERIVED_DATA_PATH/Build/Intermediates.noindex/swift-syntax.build/$CONFIGURATION*/*.build/Objects-normal/*/*.o"
+    if ls $OBJECT_FILES 1> /dev/null 2>&1; then
+        ar -crs "$LIBRARY_PATH" $OBJECT_FILES
+    else
+        echo "Error: No object files found for $ARCHS on $XCODEBUILD_PLATFORM_NAME"
+        exit 1
+    fi
 done
 
 cd ..
@@ -150,7 +150,7 @@ xcodebuild -create-xcframework \
     $XCODEBUILD_LIBRARIES \
     -output "${XCFRAMEWORK_PATH}"
 
-for ((i = 1; i < ${#PLATFORMS[@]}; i += 2)); do
+for ((i = 1; i < ${#PLATFORMS[@]}; i += 3)); do
     XCFRAMEWORK_PLATFORM_NAME="${PLATFORMS[i]}"
     OUTPUTS_PATH="${PLATFORMS_OUTPUTS_PATH}/${XCFRAMEWORK_PLATFORM_NAME}"
     cp $OUTPUTS_PATH/*.swiftinterface "$XCFRAMEWORK_PATH/$XCFRAMEWORK_PLATFORM_NAME"
